34f528f5d795b74e74e969bde0f9eb996ea0d62a
alphasec <95212748+alphasecio@users.noreply.github.com>
2025-05-14 22:57:06 +0800

    Create mcp-server-sse.py

---
+++ server/mcp-server-sse.py
@@ -0,0 +1,20 @@
+import asyncio
+from fastmcp import FastMCP
+
+mcp_server = FastMCP(name="MathMCPServer")
+
+@mcp_server.tool()
+def add(a: int, b: int) -> int:
+    """Adds two numbers"""
+    return a + b
+
+@mcp_server.tool()
+def multiply(a: int, b: int) -> int:
+    """Multiplies two numbers"""
+    return a * b
+
+async def main():
+    await mcp_server.run_async(transport="sse", path="/sse", host="0.0.0.0", port=8000)
+
+if __name__ == "__main__":
+    asyncio.run(main())
