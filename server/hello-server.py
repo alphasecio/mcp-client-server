3da28b1a55bbaa49dd1e9d70b1d3dae87578154a
alphasec <95212748+alphasecio@users.noreply.github.com>
2025-06-09 18:06:00 +0800

    Update hello-server.py

--- server/hello-server.py
+++ server/hello-server.py
@@ -5,8 +5,13 @@ mcp_server = FastMCP(name="HelloMCPServer")
 
 @mcp_server.tool()
 def greet(name: str) -> str:
-    """Greets a user by name"""
+    """Greets a user by name."""
     return f"Hello, {name}!"
+
+@mcp_server.tool
+def roll_dice(n_dice: int) -> list[int]:
+    """Rolls `n_dice` 6-sided dice and returns the results."""
+    return [random.randint(1, 6) for _ in range(n_dice)]
 
 async def main():
     await mcp_server.run_async(transport="streamable-http", path="/mcp", host="0.0.0.0", port=8000)
