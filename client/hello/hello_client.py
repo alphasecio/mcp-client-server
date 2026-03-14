b3917331a3b3ab662f37f723b5d55f9d94b3688f
alphasec <95212748+alphasecio@users.noreply.github.com>
2026-03-14 14:56:08 +0800

    Create hello_client.py

---
+++ client/hello/hello_client.py
@@ -0,0 +1,19 @@
+import os
+import asyncio
+from fastmcp import Client
+from fastmcp.client.auth import BearerAuth
+
+# Issued by Prefect Horizon for accessing MCP endpoint
+FASTMCP_URL = os.getenv("FASTMCP_URL")
+FASTMCP_API_KEY = os.getenv("FASTMCP_API_KEY")
+
+async def main():
+    async with Client(
+        FASTMCP_URL,
+        auth=BearerAuth(token=FASTMCP_API_KEY),
+    ) as client:
+
+        result = await client.call_tool("greet", {"name": "bob"})
+        print(result.data)
+
+asyncio.run(main())
